4957a4c468ddbc17f3766c890c505029a56a5fe0
AliWilkinson <96079887+AliWilkinson@users.noreply.github.com>
2022-06-19 13:44:17 +0100

    Rendered word meanings

---
+++ src/results.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Meanings from "./meanings";
+
+export default function Results(props) {
+  if (props.results) {
+    return (
+      <div className="Results">
+        <h2>{props.results.word}</h2>
+        {props.results.meanings.map((meaning, index) => {
+          return (
+            <div key={index}>
+              <Meanings meaning={meaning} />
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
